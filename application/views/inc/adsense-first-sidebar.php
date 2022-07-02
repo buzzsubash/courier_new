720d04edecd5f94aee9fb815649787cf6c185724
Subash Krishnan <buzzsubash@gmail.com>
2022-07-02 09:56:15 +0800

    Adsense sidebar

---
+++ application/views/inc/adsense-first-sidebar.php
@@ -0,0 +1,22 @@
+<div class="ads-container" style="width: 341px; height: 285px";>
+
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6247788451623601"
+			crossorigin="anonymous"></script>
+	<!-- Couriertracking.org.in - First Sidebar -->
+	<ins class="adsbygoogle"
+		 style="display:block"
+		 data-ad-client="ca-pub-6247788451623601"
+		 data-ad-slot="6207068176"
+		 data-ad-format="auto"
+		 data-full-width-responsive="false"></ins>
+	<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+
+
+</div>
+
+<br>
+
+
+
